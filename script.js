85c48c63825b71d98992a64f30e6944d0dc0fd11
Aryan <aryanbiswari14@gmail.com>
2022-12-10 12:22:14 +0530

    finally done

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const array = [
     { id: 3, name: "karen", age: "19", profession: "admin" }
 ];
 
-// Use the map function to iterate over all of the ages and make them integers
+
 const updatedArray = array.map((item) => {
     return {
         ...item,
@@ -12,7 +12,7 @@ const updatedArray = array.map((item) => {
     };
 });
 
-// Use the forEach function to edit the object with the name "john" and change his age from 18 to 19
+
 updatedArray.forEach((item) => {
     if (item.name === 'john') {
         item.age = 19;
@@ -21,7 +21,6 @@ updatedArray.forEach((item) => {
 const select = document.createElement('select');
 select.name = 'profession';
 
-// Create the options for the dropdown
 const professionOption = document.createElement('option');
 professionOption.value = 'profession';
 professionOption.textContent = 'Profession';
@@ -37,19 +36,18 @@ adminOption.value = 'admin';
 adminOption.textContent = 'Admin';
 select.appendChild(adminOption);
 
-// Create the filter button
 const button = document.createElement('button');
 button.textContent = 'Filter';
 button.classList.add('btn-class');
 button.addEventListener('click', () => {
-    // Get the selected value from the dropdown
+    
     const selectedValue = select.value;
 
     if (selectedValue && selectedValue !== "profession") {
-        // Use the filter function to filter the cards by profession
+        
         const filteredArray = updatedArray.filter((item) => item.profession === selectedValue);
 
-        // Use another forEach to render the filtered cards in the UI
+        
         filteredArray.forEach((item) => {
             const card = document.createElement('tr');
             card.innerHTML = `
@@ -58,19 +56,19 @@ button.addEventListener('click', () => {
                 <td>Profession :  ${item.profession}</td>
                 <td>Age : ${item.age}</td>`;
 
-            // Append the card to the UI
+            
             document.getElementById('cards').appendChild(card);
         });
     } else {
-        // No value was selected, show an alert
+        
         alert('Please select a profession from the dropdown before clicking the filter button.');
     }
 });
 
-// Append the dropdown and button to the DOM
+
 document.getElementById('dropdown-container').appendChild(select);
 document.getElementById('button-container').appendChild(button);
 
-// Use the spread operator to append another object to the array
+
 const newObject = {id: 4, name: "jane", age: "21", profession: "designer"};
 updatedArray.push(newObject);
